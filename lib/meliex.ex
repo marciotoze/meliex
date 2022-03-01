5595cb9021bb85784d5e4ef6a489e252e7ae5f5d
Marcio Toze <marcio.toze@gmail.com>
2022-03-01 17:47:19 -0300

    initial commit

---
+++ lib/meliex.ex
@@ -0,0 +1,18 @@
+defmodule Meliex do
+  @moduledoc """
+  Documentation for `Meliex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Meliex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/meliex_test.exs
@@ -0,0 +1,8 @@
+defmodule MeliexTest do
+  use ExUnit.Case
+  doctest Meliex
+
+  test "greets the world" do
+    assert Meliex.hello() == :world
+  end
+end
