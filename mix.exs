f93e59bfbe5d3ed67654771f530c3e353d0af393
Marcio Toze <marcio.toze@gmail.com>
2022-05-28 19:22:32 -0300

    feat: add docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Meliex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Meliex",
+      source_url: "https://github.com/marciotoze/meliex"
     ]
   end
 
@@ -21,10 +25,24 @@ defmodule Meliex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:hackney, "~> 1.18"},
       {:jason, ">= 1.3.0"},
       {:tesla, "~> 1.4"},
       {:timex, "~> 3.7.6"}
+    ]
+  end
+
+  defp description do
+    "Mercado Libre API wrapper"
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Marcio Toze"],
+      licenses: ["MIT"],
+      links: %{GitHub: "https://github.com/marciotoze/meliex"}
     ]
   end
 end
