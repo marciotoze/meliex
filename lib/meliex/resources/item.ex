562c4005afb0265f99de1691cbdb369622ce7bac
Marcio Toze <marcio.toze@gmail.com>
2022-05-29 02:48:10 -0300

    fix: calculate total pages when get products from an user'

--- lib/meliex/resources/item.ex
+++ lib/meliex/resources/item.ex
@@ -14,26 +14,36 @@ defmodule Meliex.Resources.Item do
   end
 
   def all_by_user(nickname, token) do
+    %{total: total, results: results} = get_user_products_by_page(nickname, 0, token)
+
+    total_pages = ceil(total / @limit)
+
     tasks =
-      Enum.map(0..9, fn n ->
+      Enum.map(1..total_pages, fn n ->
         Task.async(fn -> get_user_products_by_page(nickname, n, token) end)
       end)
 
-    tasks
-    |> Task.await_many()
-    |> List.flatten()
+    other_results =
+      tasks
+      |> Task.await_many()
+      |> Enum.flat_map(& &1[:results])
+
+    results ++ other_results
   end
 
   def get_user_products_by_page(nickname, page, token) do
     params = %{offset: page * @limit, limit: @limit}
 
-    {:ok, %{body: %{"results" => results}}} =
+    {:ok, %{body: body}} =
       Client.get(
         "https://api.mercadolibre.com/sites/MLB/search?nickname=#{nickname}",
         params,
         token: token
       )
 
-    Enum.map(results, & &1["id"])
+    %{
+      total: body["paging"]["total"],
+      results: Enum.map(body["results"], & &1["id"])
+    }
   end
 end
